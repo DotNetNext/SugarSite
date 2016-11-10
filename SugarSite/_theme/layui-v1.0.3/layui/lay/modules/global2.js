ea866c6b76f123c7ad7557dfaa0e16b6ae6c7285
sunkaixuan <610262374@qq.com>
2016-11-11 01:43:22 +0800

    -

--- SugarSite/_theme/layui-v1.0.3/layui/lay/modules/global2.js
+++ SugarSite/_theme/layui-v1.0.3/layui/lay/modules/global2.js
@@ -1,3 +1,4 @@
-﻿layui.define(['layer', 'code', 'form', 'element', 'util'], function (exports) {
-
+﻿layui.define(['layer', 'code', 'form', 'element', 'util', 'ejq'], function (exports) {
+    debugger
+    var test = layui.ejq;
 });
