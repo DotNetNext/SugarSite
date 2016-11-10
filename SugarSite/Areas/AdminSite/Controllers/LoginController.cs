e38d3ad74a5ac9e415baff037f709ba7cb4fe363
sunkaixuan <610262374@qq.com>
2016-11-10 23:07:15 +0800

    -

---
+++ SugarSite/Areas/AdminSite/Controllers/LoginController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SugarSite.Areas.AdminSite.Controllers
+{
+    public class LoginController : Controller
+    {
+        // GET: AdminSite/Login
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
