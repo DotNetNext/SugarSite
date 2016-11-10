05bbc31a99ba3719c2153a01aa83468a93b28a1b
sunkaixuan <610262374@qq.com>
2016-11-10 20:35:47 +0800

    init

---
+++ SugarSite/App_Start/BundleConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Web;
+using System.Web.Optimization;
+
+namespace SugarSite
+{
+    public class BundleConfig
+    {
+        public static void RegisterBundles(BundleCollection bundles)
+        {
+            
+        }
+    }
+}
---
+++ SugarSite/App_Start/Startup.Auth.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.Owin;
+using Microsoft.Owin;
+using Microsoft.Owin.Security.Cookies;
+using Microsoft.Owin.Security.Google;
+using Owin;
+
+
+namespace SugarSite
+{
+    public partial class Startup
+    {
+        public void ConfigureAuth(IAppBuilder app)
+        {
+           
+        }
+    }
+}
