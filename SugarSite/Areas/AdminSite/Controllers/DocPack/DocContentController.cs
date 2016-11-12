a1a134d95401ee3fbee3c185ed47b143ed655c79
sunkaixuan <610262374@qq.com>
2016-11-12 17:11:24 +0800

    -

--- SugarSite/Areas/AdminSite/Controllers/DocPack/DocContentController.cs
+++ SugarSite/Areas/AdminSite/Controllers/DocPack/DocContentController.cs
@@ -6,7 +6,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
-
+using SqlSugar;
 namespace SugarSite.Areas.AdminSite.Controllers
 {
     public class DocContentController : BaseController
@@ -23,30 +23,50 @@ namespace SugarSite.Areas.AdminSite.Controllers
         public ActionResult PageDCAdd()
         {
             return View();
-        } 
+        }
         #endregion
 
         #region Admin Api
         [HttpPost]
         [ValidateInput(false)]
-        public JsonResult DC_Submit(DocContent content)
+        public JsonResult DC_Submit(DocContent obj)
         {
-            var model = new ResultModel<bool>();
+            var model = new ResultModel<object>();
             _service.Command<LoginOutsourcing>((db, o) =>
             {
-                db.Insert(o);
+                var isAdd = obj.Id == 0;
+                if (isAdd)
+                {
+                    model.ResultInfo = new { id = db.Insert(obj) };
+                }
+                else
+                {
+                    db.AddDisableUpdateColumn();
+                    db.Update(obj);
+                    model.ResultInfo = new { id = obj.Id };
+                }
             });
             return Json(model);
         }
-        public JsonResult DC_Delete(int [] ids)
+        public JsonResult DC_Delete(int[] ids)
         {
             var model = new ResultModel<bool>();
             _service.Command<LoginOutsourcing>((db, o) =>
             {
-                db.FalseDelete<DocContent,int>("id",ids);
+                db.FalseDelete<DocContent, int>("id", ids);
             });
             return Json(model);
-        } 
+        }
+
+        public JsonResult DC_Single(int id)
+        {
+            var model = new ResultModel<DocContent>();
+            _service.Command<LoginOutsourcing>((db, o) =>
+            {
+                model.ResultInfo=db.Queryable<DocContent>().InSingle(id);
+            });
+            return Json(model,JsonRequestBehavior.AllowGet);
+        }
         #endregion
     }
 }
