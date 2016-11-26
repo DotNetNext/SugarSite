9b09ad0057e60ee1afe90e9f5b7b3073453617e8
sunkaixuan <610262374@qq.com>
2016-11-26 23:14:09 +0800

    -

---
+++ SugarSite/Areas/BBS/BBSAreaRegistration.cs
@@ -0,0 +1,29 @@
+﻿using System.Web.Mvc;
+
+namespace SugarSite.Areas.BBS
+{
+    public class BBSAreaRegistration : AreaRegistration 
+    {
+        public override string AreaName 
+        {
+            get 
+            {
+                return "BBS";
+            }
+        }
+
+        public override void RegisterArea(AreaRegistrationContext context) 
+        {
+            context.MapRoute(
+                "BBS_default_Index",
+                "ask",
+                new { Controller="Main", action = "Index", id = UrlParameter.Optional }
+            );
+            context.MapRoute(
+            "BBS_default",
+            "BBS/{controller}/{action}/{id}",
+            new { action = "Index", id = UrlParameter.Optional }
+        );
+        }
+    }
+}
---
+++ SugarSite/Areas/BBS/Controllers/MainController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SugarSite.Areas.BBS.Controllers
+{
+    public class MainController : Controller
+    {
+        // GET: BBS/Main
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
