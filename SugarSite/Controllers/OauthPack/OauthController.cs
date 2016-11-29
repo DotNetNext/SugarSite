94f5d1a35996d1f2e86427b8e7e04b995d8e9695
sunkaixuan <610262374@qq.com>
2016-11-29 16:23:58 +0800

    -

--- SugarSite/Controllers/OauthPack/OauthController.cs
+++ SugarSite/Controllers/OauthPack/OauthController.cs
@@ -5,24 +5,55 @@ using System.Web;
 using System.Web.Mvc;
 using SqlSugar;
 using OAuth.Tools;
+using SyntacticSugar;
+using Infrastructure.Dao;
+using Infrastructure.DbModel;
+using Infrastructure.Pub;
 
 namespace SugarSite.Controllers
 {
-    public class OauthController : Controller
+    public class OauthController : BaseController
     {
-        // GET: Oauth
+        public OauthController(DbService s) : base(s) { }
+
         public ActionResult CallBack(string state)
         {
-            if (state == "QQ")
-            {
-               
-            }
-            else {
-
-            }
             var current = OAuth2Factory.Current;
-            Response.Write(current.headUrl += " " + current.openID);
-            return View();
+            if (current.openID.IsNullOrEmpty())
+            {
+                return Content("第三方登录失败！");
+            }
+            _service.Command<OauthOutsourcing>((db, o) =>
+            {
+                var pwd = new EncryptSugar().MD5(RandomSugar.GetRandomString(10));
+                try
+                {
+                    var userMapping = db.Queryable<UserOAuthMapping>().SingleOrDefault(it => it.AppId == current.openID);
+                    if (userMapping == null)//注册
+                    {
+                        db.BeginTran();
+                        UserInfo u = o.GetUser(current, pwd);
+                        var id = db.Insert(u).ObjToInt();
+                        UserOAuthMapping um = o.GetUserOauthMapping(current, id);
+                        db.Insert(um);
+                        db.CommitTran();
+                        userMapping = um;
+                    }
+                    else
+                    {
+                        var user = db.Queryable<UserInfo>().InSingle(userMapping.UserId);
+                        var cm = CacheManager<UserInfo>.GetInstance();
+                        string uniqueKey = PubGet.GetUserKey;
+                        cm.Add(uniqueKey, user, cm.Day * 365);//保存一年
+                    }
+                }
+                catch
+                {
+                    db.RollbackTran();
+                    new Exception("第三方登录注册失败！");
+                }
+            });
+            return this.Redirect("~/ask");
         }
     }
 }
---
+++ SugarSite/Controllers/OauthPack/OauthOutsourcing.cs
@@ -0,0 +1,35 @@
+﻿using Infrastructure.DbModel;
+using OAuth.Tools;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Infrastructure.Pub;
+using SyntacticSugar;
+namespace SugarSite.Controllers
+{
+    public class OauthOutsourcing
+    {
+        public  UserInfo GetUser(OAuth2Base current, string pwd)
+        {
+            return new UserInfo()
+            {
+                Avatar = current.headUrl,
+                UserName = current.nickName,
+                NickName = current.nickName,
+                CreateTime = DateTime.Now,
+                RoleId=PubEnum.RoleType.User.TryToInt(),
+                Password = pwd
+            };
+        }
+        public UserOAuthMapping GetUserOauthMapping(OAuth2Base current, int id)
+        {
+            return new UserOAuthMapping()
+            {
+                UserId = id,
+                AppId = current.openID,
+                IsDeleted = false
+            };
+        }
+    }
+}
