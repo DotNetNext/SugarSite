0122a1b0a5ca9eea0a71cd9efc6f9ac24db9b3f8
sunkaixuan <610262374@qq.com>
2016-12-01 22:41:57 +0800

    -

---
+++ SugarSite/_theme/layui-v1.0.3/layui/lay/modules/global-bbs.js
@@ -0,0 +1,21 @@
+﻿layui.define(['layer'], function (exports) {
+    var $ = layui.jquery;
+    //查看代码
+    $(function () {
+        var w = $(window).width();
+        var $top = $(".layui-fixbar-top");
+        $top.click(function () {
+            $(document).scrollTop(0);
+        })
+        $(document).scroll(function () {
+            var dt = $(document).scrollTop();
+            if (dt > 0) {
+                $top.show();
+            } else {
+                $top.hide();
+            }
+        })
+
+    });
+    exports('global', {});
+});
