bccab09761875b51b9c94fad59a2b489752e9ea3
sunkaixuan <610262374@qq.com>
2016-12-01 17:14:24 +0800

    -

--- Infrastructure/Pub/PubConst.cs
+++ Infrastructure/Pub/PubConst.cs
@@ -19,5 +19,6 @@ namespace Infrastructure.Pub
         public const string SiteDomain = "codeisbug.com";
         public const int SitePageSize = 20;
         public const string FilterKeyFalseDelteJoin = "FalseDelteJoin";
+        public static List<int> SiteSeoFidList = new List<int>() { 9,14,11,17 };//希望收录的版块ID让文章更容易让搜索引擎识别
     }
 }
--- SugarSite/Areas/BBS/BBSAreaRegistration.cs
+++ SugarSite/Areas/BBS/BBSAreaRegistration.cs
@@ -20,9 +20,14 @@ namespace SugarSite.Areas.BBS
                 new { Controller = "Main", action = "Index", fid = UrlParameter.Optional }
             );
             context.MapRoute(
-            "BBS_default",
-            "BBS/{controller}/{action}/{id}",
-            new { action = "Index", id = UrlParameter.Optional }
+               "BBS_default_Detail",
+               "ask/{fid}/{id}",
+                new { Controller = "Main", action = "Detail", id = UrlParameter.Optional }
+            );
+            context.MapRoute(
+              "BBS_default",
+              "BBS/{controller}/{action}/{id}",
+               new { action = "Index", id = UrlParameter.Optional }
         );
         }
     }
