6ea630711f9b14f16c7385fa1a70ee9ce3817cf7
sunkaixuan <610262374@qq.com>
2016-12-02 20:03:53 +0800

    -

---
+++ SugarSite/Controllers/UserCenterPack/Outsourcing.cs
@@ -0,0 +1,21 @@
+﻿using Infrastructure.DbModel;
+using OAuth.Tools;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Infrastructure.Pub;
+using SyntacticSugar;
+using System.Net;
+using System.Drawing.Imaging;
+using System.IO;
+using SqlSugar;
+
+namespace SugarSite.Controllers
+{
+    public class UserCenterOutsourcing
+    {
+        
+  
+    }
+}
---
+++ SugarSite/Controllers/UserCenterPack/UserCenterController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SugarSite.Controllers
+{
+    public class UserCenterController : Controller
+    {
+        // GET: UserCenter
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
