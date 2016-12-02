12bc6d7d5a1e14fd90aba00d541b48940c421709
sunkaixuan <610262374@qq.com>
2016-12-03 00:05:46 +0800

    -

--- SugarSite/Controllers/UserCenterPack/UserCenterController.cs
+++ SugarSite/Controllers/UserCenterPack/UserCenterController.cs
@@ -15,6 +15,7 @@ namespace SugarSite.Controllers
     {
         public UserCenterController(DbService s) : base(s) { }
 
+        #region page
         public ActionResult Index()
         {
             if (base.IsLogin == false)
@@ -33,7 +34,9 @@ namespace SugarSite.Controllers
             });
             return View(model);
         }
+        #endregion
 
+        #region api
         public JsonResult GetPublicInfo(int id)
         {
             var model = new ResultModel<PubUserResult>();
@@ -41,8 +44,29 @@ namespace SugarSite.Controllers
             {
                 model.ResultInfo = new PubUserResult();
                 model.ResultInfo.UserInfo = db.Queryable<UserInfo>().InSingle(id);
+                //最新发布
+                model.ResultInfo.RecentAsks = db.Queryable<BBS_Topics>()
+                .Where(it => it.Posterid == id)
+                .OrderBy(it => it.Postdatetime, OrderByType.Desc)
+                .ToList();
+                //最新回复信息
+                model.ResultInfo.RecentReplies = db.Queryable<BBS_Posts>()
+               .Where(it => it.Posterid == id)
+               .OrderBy(it => it.Postdatetime, OrderByType.Desc)
+               .ToList();
+                if (model.ResultInfo.RecentReplies.IsValuable())
+                {
+                    var tids = model.ResultInfo.RecentReplies.Select(it => it.Tid).ToList();
+                    if (tids.IsValuable())
+                    {
+                        model.ResultInfo.RecentRepliesTopics = db.Queryable<BBS_Topics>()
+                       .In(it => it.Tid, tids)
+                       .ToList();
+                    }
+                }
             });
-            return Json(model,JsonRequestBehavior.AllowGet);
+            return Json(model, JsonRequestBehavior.AllowGet);
         }
+        #endregion
     }
 }
