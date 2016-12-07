8440d6f1522b45f371d66b559078f8af8a324b32
sunkaixuan <610262374@qq.com>
2016-12-07 23:22:41 +0800

    -

--- Infrastructure/Pub/PubGet.cs
+++ Infrastructure/Pub/PubGet.cs
@@ -9,9 +9,35 @@ namespace Infrastructure.Pub
 {
     public class PubGet
     {
-        public static string GetUserKey {
-            get {
-                 return CookiesManager<string>.GetInstance()[PubConst.UserUniqueKey] + "--" + PubConst.UserUniqueKey; ;
+        public static string GetUserKey
+        {
+            get
+            {
+                return CookiesManager<string>.GetInstance()[PubConst.UserUniqueKey] + "--" + PubConst.UserUniqueKey; ;
+            }
+        }
+        public static string GetEmailUserName
+        {
+            get
+            {
+                return ConfigSugar.GetAppString("EmailUserName");
+
+            }
+        }
+        public static string GetEmailPassword
+        {
+            get
+            {
+                return ConfigSugar.GetAppString("EmailPassword");
+
+            }
+        }
+        public static string GetEmailSmtp
+        {
+            get
+            {
+                return ConfigSugar.GetAppString("EmailSmtp");
+
             }
         }
     }
