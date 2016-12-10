7f84f902d595f70ed0a69443b9dd073ebcfa6c6e
sunkaixuan <610262374@qq.com>
2016-12-10 17:01:47 +0800

    -

---
+++ Infrastructure/ViewModels/UserCenter/IndexResult.cs
@@ -0,0 +1,17 @@
+﻿using Infrastructure.ViewModels.Views;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Infrastructure.ViewModels.UserCenter
+{
+    public class IndexResult
+    {
+        /// <summary>
+        /// 访客
+        /// </summary>
+        public List<V_VisitorList> VList { get; set; }
+    }
+}
---
+++ Infrastructure/ViewModels/Views/V_VisitorList.cs
@@ -0,0 +1,16 @@
+﻿using Infrastructure.DbModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Infrastructure.ViewModels.Views
+{
+    public class V_VisitorList : VisitorList
+    {
+        public string VisName { get; set; }
+
+        public string VisAvatar { get; set; }
+    }
+}
