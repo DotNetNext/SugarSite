e44b2def1162b440deae19c1927e56f34578a666
sunkaixuan <610262374@qq.com>
2016-12-18 21:45:13 +0800

    -

--- Infrastructure/Pub/PubConst.cs
+++ Infrastructure/Pub/PubConst.cs
@@ -24,7 +24,7 @@ namespace Infrastructure.Pub
         public const string SiteDomain = "codeisbug.com";
         public const string SiteMailUserName = "文档园";
         public const int SiteOnlineTimeOut = 20;//20分
-        public const int SitePageSize = 20;
+        public const int SitePageSize = 25;
         public const string FilterKeyFalseDelteJoin = "FalseDelteJoin";
         public static List<int> SiteSeoFidList = new List<int>() { 9,14,11,17 };//希望收录的版块ID让文章更容易让搜索引擎识别
     }
