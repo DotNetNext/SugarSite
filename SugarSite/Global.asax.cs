d6970d7638dc32841c1b2623bd7e8efae209887d
skx <610262374@qq.com>
2021-01-06 11:00:37 +0800

    -

--- SugarSite/Global.asax.cs
+++ SugarSite/Global.asax.cs
@@ -40,7 +40,11 @@ namespace SugarSite
             string strUrl = Request.Url.ToString().Trim().ToLower();
             if (strUrl.Contains("codeisbug"))
             {
-                Response.RedirectPermanent(strUrl.Replace("codeisbug","donet5"));
+                var url = strUrl.Replace("codeisbug", "donet5");
+                HttpContext.Current.Response.ContentType = "text/html;charset=utf-8";
+                HttpContext.Current.Response.ContentEncoding = System.Text.Encoding.GetEncoding("utf-8"); //设置输出流为简体中文
+                Response.Write("<html><title>孙凯旋个人主页</title> <body><p>孙凯旋个人主页</p> 该域名已停止使用 新地址  <a href='"+ url + "'> 点这里 </a> <p>当前网站备案号： 苏ICP备16056174号-1 </p> </html> </body>");
+                Response.End();
             }
             if (strUrl.Contains(PubConst.SiteDomain.ToLower()) && !strUrl.Contains("www."))
             {
